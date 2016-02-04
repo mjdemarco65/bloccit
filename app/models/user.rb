6dd68bea54710756705f6d661261e2ab49c639ba
Michael DeMarco <michael.demarco65@gmail.com>
2016-02-04 15:14:35 -0700

    36 assignment
    Added callback to split, uppercase first letter of first and last and
    join back together.

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ActiveRecord::Base
+
+  before_save { self.email = email.downcase }
+  before_save :format_name
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 100 },
+            format: { with: EMAIL_REGEX }
+
+  has_secure_password
+
+    def format_name
+      if name
+        name_array = []
+        name.split.each do |first_last|
+        name_array << first_last.capitalize
+    end
+    self.name = name_array.join(" ")
+    end
+  end
+end
